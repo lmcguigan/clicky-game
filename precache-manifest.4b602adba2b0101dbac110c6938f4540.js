28819e870e16264e921430eee2fe4258ce5e7a70
Lucy M <lmcguigan@users.noreply.github.com>
2018-12-04 09:46:09 -0600

    Updates

---
+++ precache-manifest.4b602adba2b0101dbac110c6938f4540.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "ec334e2be537002a2ada",
+    "url": "/clicky-game/static/js/main.ec334e2b.chunk.js"
+  },
+  {
+    "revision": "a058946296ae0a9270bb",
+    "url": "/clicky-game/static/js/1.a0589462.chunk.js"
+  },
+  {
+    "revision": "ec334e2be537002a2ada",
+    "url": "/clicky-game/static/css/main.4f2d1337.chunk.css"
+  },
+  {
+    "revision": "b17fe0768a74fa5906b540fb5f1b5be0",
+    "url": "/clicky-game/index.html"
+  }
+];
